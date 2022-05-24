ec182bc91fd638d46f72abc5aefc0483a002de17
Joshua Graham <sc21jg@leeds.ac.uk>
2022-05-25 02:00:09 +0300

    more testscript changes

--- assignment_2/testscript.sh
+++ assignment_2/testscript.sh
@@ -147,11 +147,15 @@ else
 fi
 
 echo -n "Test 13 - gtopoPrintLand: "
-./gtopoPrintLand /vol/scratch/SoC/COMP1921/COMP1921A2_handout/dems/gtopo30full_reduced_216.dem 200 100 nobackup/aa_printedGtopo30full_reduced_216 -9000 100 500)
+RESULT=$(./gtopoPrintLand /vol/scratch/SoC/COMP1921/COMP1921A2_handout/dems/gtopo30full_reduced_216.dem 200 100 nobackup/aa_printedGtopo30full_reduced_216 -9000 100 500)
+VALUE=$?
+echo -e "${RESULT}, code=${VALUE}"
 
 
 echo -n "Test 14 - gtopoReduceAssemble view: "
-bash ./COMP1921A2_handout/gtopoReduce400.sh
+RESULT=$(bash ./COMP1921A2_handout/gtopoReduce400.sh)
+VALUE=$?
+echo -e "${RESULT}, code=${VALUE}"
 
 #----------- Testing invalid command line arguments ----------#
 echo -n -e "* TESTING INVALID COMMMAND LINE ARGUMENTS *\n"
